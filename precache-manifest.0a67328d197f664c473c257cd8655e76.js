acaf7054813811a2298f8b3fb57c0d24991b71b7
dong-geon <wge3142@gmail.com>
2020-07-09 11:08:37 +0900

    Updates

---
+++ precache-manifest.0a67328d197f664c473c257cd8655e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36950912fb435db78e08066e975d865",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "e2655ad4cc80bc79d62f",
+    "url": "/todo-list/static/css/main.10f30028.chunk.css"
+  },
+  {
+    "revision": "307227dbbc043e3fe105",
+    "url": "/todo-list/static/js/2.3b50ce1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.3b50ce1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2655ad4cc80bc79d62f",
+    "url": "/todo-list/static/js/main.16cf8f8a.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
